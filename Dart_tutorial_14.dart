cec7f98fb12b77669bc5b1b15eec4cb1525b9f36
Abdur Razzaq <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-21 21:17:15 -0700

    Relational and equality operators

---
+++ Dart_tutorial_14.dart
@@ -0,0 +1,19 @@
+void main() {
+  int a = 30;
+	int b = 30;
+	
+	bool smaller, greater, equal, smallerorequal, greaterorequal, notequal;
+	smaller = a > b;
+	greater = a < b;
+	equal = a == b;
+	smallerorequal = a <= b;
+	greaterorequal = a >= b;
+	notequal = a != b;
+	
+	print (smaller);
+	print (greater);
+	print (equal);
+	print (smallerorequal);
+	print (greaterorequal);
+	print (notequal);
+}
