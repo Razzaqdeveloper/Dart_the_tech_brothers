fd001bedffc4286e011866617da6aba12686febf
Abdur Razzaq <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-23 23:03:39 -0700

    If_Else statement in dart

---
+++ Dart_tutorial_16-1.dart
@@ -0,0 +1,17 @@
+void main() {
+  int x = 3;
+  int y = 10;
+	// if(x % 2 == 0){
+	// 	print("$x is even number");
+	// }
+	// else{
+	// 	print("$x is odd number");
+	// }
+
+	if(x > y){
+		print("$x is greater than $y");
+	}
+	else{
+		print("$x is smaller than $y");
+	}
+}
