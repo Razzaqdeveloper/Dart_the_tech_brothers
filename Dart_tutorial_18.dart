61c27ae19ed9997ff96255074d877d54702091cc
Abdur Razzaq <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-24 22:44:35 -0700

    Increment and Decrement operators

---
+++ Dart_tutorial_18.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+int x = 10;
+
+	//there are two types 
+	
+	// Prefix, Postfix
+	// x++; // postfix increment
+	// x--; // postfix decrement
+
+	++x; //prefix increment
+	--x; //prefix decrement
+
+	print(x);
+}
